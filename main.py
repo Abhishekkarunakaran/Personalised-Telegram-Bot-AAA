8d2acaf5f8ef98be42e756c9ce9d30c7696f07f0
Abhishekkarunakaran <abhishekkarunakaran100@gmail.com>
2022-07-10 12:45:28 +0530

    Added inline keyboard feature:
    Added the feature to collection data from the user
    main.py:
    * prg_Markup() => Returns a markup with programs as inline keyboard
      buttons - written using for-loop
    * sem_Markup(sem) => sem - integer value which coresponse to the program
      previously entered.
    * callback_query_handler => It handles the inline keyboard button onTap
      events.
      In this fn(), we insert & update data in db.
    
    requirements.txt
    generated using the package pipreqs (command "pipreqs --force")
    
    prg_sem_data.json
    * carries program name as key and the no.of semesters as values for
      coresponding programs

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # import requests
 # from bs4 import BeautifulSoup
 # import json
+# import re
+import json
+from msilib.schema import Error
+import re
 import telebot
 from scrapper import scrap
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -9,18 +13,15 @@ bot_token = "5333091432:AAEE-MtnYqdrHo1N09LRBHIFW_BexsCs2pQ"
 
 chat_id = ["865161907", "722830299", "735059361"]
 
-# prgm=  "*Choose your Programme*\n/B.Tech\n/B.Arch\n/MCA\n/M.Tech"
-
-# sem_btech = ""
 
 bot = telebot.TeleBot(bot_token)
 parse_mode = 'MARKDOWN'
 
 
-@bot.message_handler(commands=['new'])
-def sendMessage(message):
-    msg = scrap()
-    bot.send_message(message.chat.id, msg, parse_mode)
+# @bot.message_handler(commands=['new'])
+# def sendMessage(message):
+#     msg = scrap()
+#     bot.send_message(message.chat.id, msg, parse_mode)
 
 
 # A new feature :-
@@ -28,11 +29,11 @@ def sendMessage(message):
 # then the scrapper scrapes the n-th notification
 
 
-@bot.message_handler(content_types=['text'])
-def nthNotif(message):
-    print(message.text)
-    msg = scrap(int(message.text) - 1)
-    bot.send_message(message.chat.id, msg, parse_mode)
+# @bot.message_handler(content_types=['text'])
+# def nthNotif(message):
+#     print(message.text)
+#     msg = scrap(int(message.text) - 1)
+#     bot.send_message(message.chat.id, msg, parse_mode)
 
 
 # This needs any message #########
@@ -46,35 +47,63 @@ def nthNotif(message):
 
 #InlineKeyboard
 
+with open('prg_sem_data.json') as json_file:
+    prg_sem = json.load(json_file)
 
 def prg_Markup():
-    markup = InlineKeyboardMarkup()
-    markup.width = 2
-    markup.add(
-        InlineKeyboardButton("B.Tech", callback_data="B.Tech"),
-        InlineKeyboardButton("B.Arch", callback_data="B.Arch"),
-        InlineKeyboardButton("B.Des", callback_data="B.Des"),
-        InlineKeyboardButton("M.Tech", callback_data="M.Tech"),
-        InlineKeyboardButton("MCA", callback_data="MCA"),
-        InlineKeyboardButton("M.Arch", callback_data="M.Arch"),
-        InlineKeyboardButton("MBA", callback_data="MBA"),
-    )
+    markup = InlineKeyboardMarkup(row_width= 3)
+    button_list = [
+        InlineKeyboardButton(prg, callback_data=prg) for prg in prg_sem
+    ]
+    markup.add(*button_list)
     return markup
 
+
+def sem_Markup(sem):
+    markup = InlineKeyboardMarkup(row_width = 4) 
+    button_list = [
+        InlineKeyboardButton("S{}".format(i), callback_data="S{}".format(i)) for i in range(1,sem+1)
+    ]
+    markup.add(*button_list)
+    # markup.
+    return markup
 
 @bot.message_handler(commands=['start'])
 def greet(message):
     bot.send_message(message.chat.id, "*Welcome to\nKTU Personal Updates Bot*",
                      parse_mode)
     bot.send_message(message.chat.id,
-                     "Select your program",
-                     reply_markup=prg_Markup)
-
+                     "Select your *Program*",
+                     parse_mode,
+                     reply_markup=prg_Markup())
+   
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-    print(call.data)
+    
+    if (call.data in prg_sem):
+        #TODO: fn() - enter the chat.id and program to the db
+        
+        #? inst_db(call.from_user.id,call.data)
 
+        bot.answer_callback_query(call.id,"Saved your program {}".format(call.data))
+        
+        try :
+            bot.send_message(call.from_user.id,
+                     "Select your *Semester*",
+                     parse_mode,
+                     reply_markup = sem_Markup(prg_sem[call.data]))
+        except Exception as e:
+            print(e)
+            
+    if(re.search("S*",call.data)):
+        #TODO: fn() - enter the chat.id and sem into db
+
+        #? updt_db(call.from_user.id,call.data)
+
+        bot.answer_callback_query(call.id,"Saved your semester {}".format(call.data))
+    
+    
 
 def main():
     bot.polling()
